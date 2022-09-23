dc138da2ee0fff187a8ee63b52e5f99743e76dfe
Mike Casey <mike.casey22>
2022-09-23 15:12:04 -0500

    changed api address, removed v1 from url

--- lib/orders/getOrderDetails.ts
+++ lib/orders/getOrderDetails.ts
@@ -13,7 +13,7 @@ export default async function getOrderDetails(
 ): Promise<Order> {
   await IdSchema.required().validateAsync(orderId);
   try {
-    const path = `/orders/salesorder/v1/${orderId}`;
+    const path = `/orders/salesorder/${orderId}`;
     const headers = await this.signRequest("GET", path);
     if (!headers) throw new Error("Could not sign request");
     const res = await this.http.get(path, { headers });
